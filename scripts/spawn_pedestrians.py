acbc70ae67eaa90509e609d799053429d8e81c6a
Adel <mohamed.adel03@gmail.com>
2019-03-18 01:04:54 +0100

    adding motion to pedestrians

--- scripts/spawn_pedestrians.py
+++ scripts/spawn_pedestrians.py
@@ -24,7 +24,12 @@ def main():
     ped_blueprints = world.get_blueprint_library().filter("walker.*")
     spawn_points = list(world.get_map().get_spawn_points())
     for i in range(1,len(spawn_points),1):
-        world.try_spawn_actor(random.choice(ped_blueprints), spawn_points[i])
+        player = world.try_spawn_actor(random.choice(ped_blueprints), spawn_points[i])
+        player_control = carla.WalkerControl()
+        player_control.speed = 3.
+        player_rotation = carla.Rotation(0,90,0)
+        player_control.direction = player_rotation.get_forward_vector()
+        player.apply_control(player_control)
         logging.debug("spawned pedestrian at {}".format(str(spawn_points[i])))
 
 if __name__ == '__main__':
